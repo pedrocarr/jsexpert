970975921f45f7f2e7f6ecd73da06d225e67657f
Pedro Carvalho <eng.pedro.amb@gmail.com>
2022-02-15 10:44:12 -0300

    feat: set up mocks

--- class01-mocks/src/file.js
+++ class01-mocks/src/file.js
@@ -1,9 +1,14 @@
-const { readFile } = require('fs/promises')
+const { readFile } = require('fs').promises
 const { join } = require('path')
+const DEFAULT_OPTION = {
+    maxLines: 3,
+    fields: ["id","name","profession","age"]
+}
 
 class File {
     static async csvToJson(filePath) {
         const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
         return content
     }
 
@@ -12,12 +17,20 @@ class File {
         return (await readFile(fileName)).toString("utf8")
     }
 
-    static getFileContent(filePath) {
-
+    static isValid(csvString, options = DEFAULT_OPTION) {
+        const [header, ...fileWithoutHeader] = csvString.split('\n')
+        const isHeaderValid = header === options.fields.join(',')
+        if(!isHeaderValid) {
+            return {
+                error:
+            }
+        }
     }
+
 
 }
 (async () => {
-    const result = await File.csvToJson("./../mocks/threeItems-valid.csv")
+    // const result = await File.csvToJson("./../mocks/threeItems-valid.csv")
+    const result = await File.csvToJson("./../mocks/fourItems-invalid.csv")
     console.log('result', result)
 })();
