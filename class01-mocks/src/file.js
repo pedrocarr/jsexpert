2e6db3effecf6ee24efab1cd00db83fb237b75b5
Pedro Carvalho <eng.pedro.amb@gmail.com>
2022-02-15 09:58:54 -0300

    feat: set up mocks

--- class01-mocks/src/file.js
+++ class01-mocks/src/file.js
@@ -1,6 +1,19 @@
+const { readFile } = require('fs/promises')
+const { join } = require('path')
+
 class File {
     static async csvToJson(filePath) {
-        return 'Hello!'
+        const content = await File.getFileContent(filePath)
+        return content
+    }
+
+    static async getFileContent(filePath) {
+        const fileName = join(__dirname, filePath)
+        return (await readFile(fileName)).toString("utf8")
+    }
+
+    static getFileContent(filePath) {
+
     }
 
 }
